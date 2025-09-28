1270a0ac5b162ce205f7f92fb9f0e25466fd330c
Abdu-khaled <abdelrahmankhalid27@gmail.com>
2025-09-29 00:29:40 +0300

    remove region in variables vpc modules

--- modules/VPC/variables.tf
+++ modules/VPC/variables.tf
@@ -8,11 +8,6 @@ variable "environment" {
   type        = string
 }
 
-# variable "region" {
-#   description = "AWS region"
-#   type        = string
-# }
-
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
